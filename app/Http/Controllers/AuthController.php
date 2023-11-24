96a68d2222cdcfd17858474edb62e7fe5f5b61d9
Mohammad Nasyikh Al-Qusyairy <nasyikhqusyairy@gmail.com>
2023-11-24 17:55:32 +0700

    new file:   .rnd 	new file:   app/Http/Controllers/AuthController.php 	new file:   app/Http/Controllers/SponsorController.php 	new file:   app/Models/Sponsor.php 	modified:   app/Models/User.php 	modified:   app/Providers/AppServiceProvider.php 	modified:   composer.json 	modified:   composer.lock 	modified:   config/auth.php 	new file:   database/migrations/2023_11_24_105033_create_sponsors_table.php 	modified:   routes/api.php

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        // Validasi data yang diterima dari request
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+
+        $token = $user->createToken('AppName')->accessToken;
+
+        return response()->json(['token' => $token], 200);
+    }
+
+    public function login(Request $request)
+    {
+        $credentials = request(['email', 'password']);
+
+        if (!Auth::attempt($credentials)) {
+            return response()->json(['message' => 'Invalid Credentials'], 401);
+        }
+
+        $user = User::find(auth()->user()->id);
+        $token = $user->createToken('user')->accessToken;
+
+        return response()->json(['token' => $token, 'user' => $user], 200);
+    }
+    public function logout(Request $request)
+    {
+        $request->user()->tokens()->delete();
+
+        return response()->json(['message' => 'Successfully logged out'], 200);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CategoryController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -18,5 +19,14 @@ use Illuminate\Support\Facades\Route;
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //     return $request->user();
 // });
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::middleware('auth:api')->post('/logout', [AuthController::class, 'logout']);
+
+// Contoh route yang memerlukan autentikasi
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
 
 Route::apiResource('categories', CategoryController::class);
